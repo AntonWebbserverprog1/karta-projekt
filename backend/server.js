7e95a923efe7f9ca67a001d2fe35b040ee69467f
AntonWebbserverprog1 <anton.levanderhubinette@elev.ga.ntig.se>
2026-03-13 20:48:52 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,8 @@ const JWT_SECRET = "min_hemliga_nyckel_123";
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 // --- AUTH MIDDLEWARE ---
 const authenticateToken = (req, res, next) => {
